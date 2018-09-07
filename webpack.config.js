bed3145aaf8532ccfb269942bb765db6d6936cfb
Ernesto Jiménez Villaseñor <ernesto@bedu.org>
2018-09-07 15:55:20 -0500

    Added webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    entry: {
+        app: './index.js'
+    },
+    output: {
+        filename: '[name]-[hash:8]-bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html/,
+                exclude: /node_modules/,
+                use: 'html-loader'
+            },
+            {
+                test: /\.js/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({template: './index.html'})
+    ]
+}
